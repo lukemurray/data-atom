d59e07252f1c2c53f8dac5a9e9894dc4212c182a
Dustin Speckhals <dustin1114@gmail.com>
2016-08-22 15:50:52 -0400

    Add column autocompletion for insert statements
    This feature adds autocompletion for SQL insert statements by giving
    table columns as autocomplete suggestions, as well as adding an "ALL"
    suggestion that adds all column names in column order, comma separated.
    If columns have already been typed/autocompleted, they will not show in
    the autocomplete suggestions.

--- spec/meta-provider-spec.js
+++ spec/meta-provider-spec.js
@@ -1,6 +1,34 @@
 'use babel';
 
-import SQLMetaProvider from '../lib/sql-meta-provider';
+import InsertMetaProvider from '../lib/providers/insert-meta-provider';
+import SQLMetaProvider from '../lib/providers/sql-meta-provider';
+
+describe('InsertMetaProvider', () => {
+  describe('Get columns', () => {
+  let editor = {
+    dataAtomColumns: [{
+        name: 'test_col',
+        tableName: 'tbl',
+        type: 'varchar'
+      },
+      {
+        name: 'other_col',
+        tableName: 'tbl',
+        type: 'bool'
+      }]
+    };
+    it('filters by table name including the ALL suggestion when no previous columns exist', () => {
+        let cols = InsertMetaProvider.getTableColumnNames(editor, 'tbl', []);
+        expect(cols).toContain({text: 'test_col', rightLabelHTML: '<span class="data-atom autocomplete autocomplete-col"></span>Column', leftLabel: 'varchar'});
+        expect(cols.length).toEqual(3);
+    });
+    it('filters by table name and only includes columns that are not existing already', () => {
+        let cols = InsertMetaProvider.getTableColumnNames(editor, 'tbl', ['test_col']);
+        expect(cols).toContain({text: 'other_col', rightLabelHTML: '<span class="data-atom autocomplete autocomplete-col"></span>Column', leftLabel: 'bool'});
+        expect(cols.length).toEqual(1);
+    });
+  })
+});
 
 describe('SQLMetaProvider', () => {
   describe('Get table names', () => {
